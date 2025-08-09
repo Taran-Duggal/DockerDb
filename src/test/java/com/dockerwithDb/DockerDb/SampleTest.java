f05c1af44b0dd8bdac02a2cc8cb67ef51102fe19
duggal.84 <Taran.Duggal@teleperformancedibs.com>
2025-08-09 23:29:18 +0530

    Jenkinsfile

---
+++ src/test/java/com/dockerwithDb/DockerDb/SampleTest.java
@@ -0,0 +1,12 @@
+package com.dockerwithDb.DockerDb;
+
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+    @Test
+    public void testSomething() {
+        assert true;
+    }
+}
+
