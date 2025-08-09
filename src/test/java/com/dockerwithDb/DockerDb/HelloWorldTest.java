7df15d811ffb086d599a27f6568e51491118044e
duggal.84 <Taran.Duggal@teleperformancedibs.com>
2025-08-09 23:38:11 +0530

    changes

---
+++ src/test/java/com/dockerwithDb/DockerDb/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.dockerwithDb.DockerDb;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void testHello() {
+        System.out.println("Hello from test!");
+    }
+}
+
